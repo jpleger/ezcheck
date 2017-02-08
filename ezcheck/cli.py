4acafce6ecc009adff652d4e2c8377c6a7b96caa
jpleger <jpleger@gmail.com>
2017-02-07 21:59:01 -0700

    CLI options

--- ezcheck/cli.py
+++ ezcheck/cli.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import logging
 import click
+from ezcheck import download_ffl_db
 logger = logging.getLogger('ezcheck')
 logger.setLevel(logging.DEBUG)  # Collect all log levels
 logger.addHandler(logging.NullHandler())  # Set the default logger to be a nullhandler
@@ -9,11 +10,11 @@ logger.addHandler(logging.NullHandler())  # Set the default logger to be a nullh
 @click.command()
 @click.option('--ffl', envvar='FFL_NUMBER', prompt=True)
 @click.option('--filename')
-def download_ffl_list(ffl, filename=None):
+def download_ffl_database(ffl, filename=None):
     """Download FFL List from ATF.gov"""
     click.echo("Starting Download using eZCheck")
-    click.echo(ffl)
-    click.echo(filename)
+    file_path = download_ffl_db(ffl, filename)
+    click.echo("Downloaded FFL Database to: %s" % file_path)
 
 if __name__ == '__main__':
-    download_ffl_list()
+    download_ffl_database()
