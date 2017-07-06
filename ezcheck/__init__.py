768382a70b7de419e5b88693c10fbe2a211ac111
James Pleger <jpleger@jpleger-mb.local>
2017-07-06 16:37:06 -0700

    Adding imports, so they don't have to be explicit

--- ezcheck/__init__.py
+++ ezcheck/__init__.py
@@ -3,3 +3,6 @@
 __author__ = """James Pleger"""
 __email__ = 'jpleger@gmail.com'
 __version__ = '0.2.1'
+
+# Don't want to import from the lib twice (ezcheck.ezcheck)
+from ezcheck import download_ffl_db, parse_row, parse_ffl_number, parse_zipcode
