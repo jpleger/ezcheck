4a4985b5063c9f48661299e06c816866ca90b001
James Pleger <jpleger@jpleger-mb.local>
2017-07-06 16:36:29 -0700

    Remove replacement/formatting for cli app

--- ezcheck/cli.py
+++ ezcheck/cli.py
@@ -14,7 +14,7 @@ def download_ffl_database():
     parser.add_argument("filename", help="filename to download file to")
     args = parser.parse_args()
     print "Downloading FFL"
-    ffl = str(args.ffl).replace('-','')
+    ffl = args.ffl
     file_path = download_ffl_db(ffl, args.filename)
     print "Downloaded FFL Database to: %s" % file_path
 
