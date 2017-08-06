fd80159248d1f04a75d2386d46a12b7d373f8e78
James Pleger <jpleger@jpleger-mb.local>
2017-08-05 17:21:45 -0700

    Bump version: 0.3.0 → 0.4.0

--- ezcheck/__init__.py
+++ ezcheck/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = """James Pleger"""
 __email__ = 'jpleger@gmail.com'
-__version__ = '0.3.0'
+__version__ = '0.4.0'
 
 # Don't want to import from the lib twice (ezcheck.ezcheck)
 from ezcheck import download_ffl_db, parse_row, parse_ffl_number, parse_zipcode
